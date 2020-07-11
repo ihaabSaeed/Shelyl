cdee29a7b2147708d48217764585af0115007a48
ihaabSaeed <ihaabsaid@gmail.com>
2020-07-11 12:51:51 +0200

    k

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname , '../public')
+
+ app.get("/Signup",function(req,res){
+ 	res.sendFile(__dirname+"/Signup.html")
+})
 app.use(express.static(publicDirectoryPath))
 app.listen(port,()=>{
 	console.log('Server on 3000 avialible' + port)
